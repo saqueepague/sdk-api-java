a14ef500a14d0da2f2012fcdba53fc4de3fb5d00
Luiz Leite <luiz.leite@saqueepague.com.br>
2020-05-27 11:06:12 -0300

    Ajustes processo Deposito Varejistas

--- src/main/java/io/swagger/configuration/SwaggerDocumentationConfig.java
+++ src/main/java/io/swagger/configuration/SwaggerDocumentationConfig.java
@@ -20,7 +20,7 @@ public class SwaggerDocumentationConfig {
             .license("")
             .licenseUrl("http://unlicense.org")
             .termsOfServiceUrl("http://www.saqueepague.com.br")
-            .version("1.19.3")
+            .version("1.19.4")
             .contact(new Contact("","", ""))
             .build();
     }
