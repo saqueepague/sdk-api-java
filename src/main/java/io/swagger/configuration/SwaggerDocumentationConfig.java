59ae68c48b4c493751dcb69dd4208b8451c2ecde
João Pedro Gomes de Brito <jb63006@saqueepague.com.br>
2021-08-26 15:47:38 -0300

    Small fixes on the loan transaction

--- src/main/java/io/swagger/configuration/SwaggerDocumentationConfig.java
+++ src/main/java/io/swagger/configuration/SwaggerDocumentationConfig.java
@@ -20,7 +20,7 @@ public class SwaggerDocumentationConfig {
             .license("")
             .licenseUrl("http://unlicense.org")
             .termsOfServiceUrl("http://www.saqueepague.com.br")
-            .version("1.25.1")
+            .version("1.25.2")
             .contact(new Contact("","", ""))
             .build();
     }
