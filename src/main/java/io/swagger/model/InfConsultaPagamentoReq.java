38365243b260bbc9ca50d7fc60f8f9a83c1e98b3
dc22094 <dc22094@saqueepague.com.br>
2022-05-26 11:45:12 -0300

    Small fixes in the swagger documentation

--- src/main/java/io/swagger/model/InfConsultaPagamentoReq.java
+++ src/main/java/io/swagger/model/InfConsultaPagamentoReq.java
@@ -21,6 +21,12 @@ public class InfConsultaPagamentoReq   {
   @JsonProperty("cnpj")
   private String cnpj = null;
 
+  @JsonProperty("numAgencia")
+  private String numAgencia = null;
+
+  @JsonProperty("numConta")
+  private String numConta = null;
+
   @JsonProperty("codBarras")
   private InfConsultaPagamentoReqCodBarras codBarras = null;
 
@@ -62,6 +68,44 @@ public class InfConsultaPagamentoReq   {
     this.cnpj = cnpj;
   }
 
+  public InfConsultaPagamentoReq numAgencia(String numAgencia) {
+    this.numAgencia = numAgencia;
+    return this;
+  }
+
+  /**
+   * Número da agência utilizada na operação.
+   * @return numAgencia
+  **/
+  @ApiModelProperty(example = "4029", value = "Número da agência utilizada na operação.")
+
+@Pattern(regexp="^\\d+$")   public String getNumAgencia() {
+    return numAgencia;
+  }
+
+  public void setNumAgencia(String numAgencia) {
+    this.numAgencia = numAgencia;
+  }
+
+  public InfConsultaPagamentoReq numConta(String numConta) {
+    this.numConta = numConta;
+    return this;
+  }
+
+  /**
+   * Número da conta utilizada na operação.
+   * @return numConta
+  **/
+  @ApiModelProperty(example = "0082348296", value = "Número da conta utilizada na operação.")
+
+@Pattern(regexp="^\\d+$")   public String getNumConta() {
+    return numConta;
+  }
+
+  public void setNumConta(String numConta) {
+    this.numConta = numConta;
+  }
+
   public InfConsultaPagamentoReq codBarras(InfConsultaPagamentoReqCodBarras codBarras) {
     this.codBarras = codBarras;
     return this;
@@ -94,12 +138,14 @@ public class InfConsultaPagamentoReq   {
     InfConsultaPagamentoReq infConsultaPagamentoReq = (InfConsultaPagamentoReq) o;
     return Objects.equals(this.cpf, infConsultaPagamentoReq.cpf) &&
         Objects.equals(this.cnpj, infConsultaPagamentoReq.cnpj) &&
+        Objects.equals(this.numAgencia, infConsultaPagamentoReq.numAgencia) &&
+        Objects.equals(this.numConta, infConsultaPagamentoReq.numConta) &&
         Objects.equals(this.codBarras, infConsultaPagamentoReq.codBarras);
   }
 
   @Override
   public int hashCode() {
-    return Objects.hash(cpf, cnpj, codBarras);
+    return Objects.hash(cpf, cnpj, numAgencia, numConta, codBarras);
   }
 
   @Override
@@ -109,6 +155,8 @@ public class InfConsultaPagamentoReq   {
     
     sb.append("    cpf: ").append(toIndentedString(cpf)).append("\n");
     sb.append("    cnpj: ").append(toIndentedString(cnpj)).append("\n");
+    sb.append("    numAgencia: ").append(toIndentedString(numAgencia)).append("\n");
+    sb.append("    numConta: ").append(toIndentedString(numConta)).append("\n");
     sb.append("    codBarras: ").append(toIndentedString(codBarras)).append("\n");
     sb.append("}");
     return sb.toString();
