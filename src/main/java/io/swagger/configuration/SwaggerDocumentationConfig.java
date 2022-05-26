7e41b08b7ba4998b1a97b8bf2a453d95eca6227d
dc22094 <dc22094@saqueepague.com.br>
2022-05-26 12:06:13 -0300

    Small fixes in the swagger documentation

--- src/main/java/io/swagger/configuration/SwaggerDocumentationConfig.java
+++ src/main/java/io/swagger/configuration/SwaggerDocumentationConfig.java
@@ -20,7 +20,7 @@ public class SwaggerDocumentationConfig {
             .license("")
             .licenseUrl("http://unlicense.org")
             .termsOfServiceUrl("http://www.saqueepague.com.br")
-            .version("1.27.2")
+            .version("1.27.3")
             .contact(new Contact("","", ""))
             .build();
     }
